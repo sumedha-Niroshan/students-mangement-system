b6e54821421ff9039dd8f6b180e78a8bf3ef098c
sumedha Niroshan <sumedhan199@gmail.com>
2024-01-09 08:16:32 +0530

    Create function on signup page

--- src/com/devstack/eduManage/controller/SignupFormController.java
+++ src/com/devstack/eduManage/controller/SignupFormController.java
@@ -1,4 +1,45 @@
 package com.devstack.eduManage.controller;
 
+import com.devstack.eduManage.db.Database;
+import com.devstack.eduManage.model.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class SignupFormController {
+    public TextField txtLastName;
+    public TextField txtPassword;
+    public AnchorPane context;
+    public TextField txtFirstName;
+    public TextField txtEmail;
+
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
+    }
+
+    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
+
+        String email = txtEmail.getText().toLowerCase();
+        String firstName = txtFirstName.getText();
+        String lastName = txtLastName.getText();
+        String password = txtPassword.getText().trim();
+        Database.userTable.add(
+                new User(firstName,lastName,email,password)
+        );
+        new Alert(Alert.AlertType.INFORMATION,"Welcome").show();
+        setUi("LoginForm");
+    }
+
+    private  void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/"+location+".fxml")))));
+        stage.centerOnScreen();
+    }
 }
--- src/com/devstack/eduManage/db/Database.java
+++ src/com/devstack/eduManage/db/Database.java
@@ -1,4 +1,9 @@
 package com.devstack.eduManage.db;
 
+import com.devstack.eduManage.model.User;
+
+import java.util.ArrayList;
+
 public class Database {
+    public  static ArrayList<User> userTable = new ArrayList();
 }
